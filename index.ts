9f22f53509f24c41c92a395d2ba29039383da09f
heeyeop <ssucgoo@gmail.com>
2025-04-19 17:51:13 +0900

    :sparkles: s3 event 를 받는 sqs 구성

--- index.ts
+++ index.ts
@@ -3,8 +3,28 @@ import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
 
+const CONTENTS_STAND_BY_QUEUE_NAME = process.env.CONTENTS_STAND_BY_QUEUE ?? "";
 const CONTENTS_BUCKET_NAME = process.env.CONTENTS_BUCKET ?? "";
 
 const contentsBucket = new aws.s3.BucketV2(CONTENTS_BUCKET_NAME);
+
+const contentsStandByQueue = new aws.sqs.Queue(CONTENTS_STAND_BY_QUEUE_NAME, {
+    policy: aws.iam.getPolicyDocumentOutput({
+        statements: [{
+            effect: "Allow",
+            principals: [{
+                type: "*",
+                identifiers: ["*"],
+            }],
+            actions: ["sqs:SendMessage"],
+            resources: [`arn:aws:sqs:*:*:${CONTENTS_STAND_BY_QUEUE_NAME}-${pulumi.getStack()}`],
+            conditions: [{
+                test: "ArnEquals",
+                variable: "aws:SourceArn",
+                values: [contentsBucket.arn]
+            }]
+        }],
+    }).apply(policy => policy.json),
+});
 
 export const bucketName = contentsBucket.id;
